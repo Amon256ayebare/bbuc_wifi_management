03e341686cc24f9340aef74d5b3c05a9f2c21dde
Amon256ayebare <Amonayebare02@gmail.com>
2025-12-29 22:08:48 +0300

    Create force_admin.php to insert admin user

---
+++ force_admin.php
@@ -0,0 +1,10 @@
+<?php
+require 'db.php';
+
+$email = "amonayebare02@gmail.com";
+$password = password_hash("admin123", PASSWORD_DEFAULT);
+
+$stmt = $pdo->prepare("INSERT INTO admins (email, password) VALUES (?, ?)");
+$stmt->execute([$email, $password]);
+
+echo "Admin inserted successfully";
